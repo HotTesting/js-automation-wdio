fd482fcdeea8aea5d2f260b667d627d3bb0ca6c5
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-02-18 16:24:29 +0200

    Updated to v5

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,23 +1,27 @@
-process.env.TS_NODE_FILES = true
-require('ts-node').register()
-
 exports.config = {
-    specs: [
-        './tests/**/*.ts'
-    ],
-    port: '9515',
-    path: '/',
-    services: ['chromedriver'],
-    capabilities: [{
-        browserName: 'chrome'
-    }],
-    sync: true,
-    logLevel: 'silent',
-    coloredLogs: true,
-    deprecationWarnings: true,
-    baseUrl: 'https://movies-finder.firebaseapp.com/',
-    framework: 'mocha',
-    mochaOpts: {
-        ui: 'bdd'
+  // hostname: "localhost",
+  // port: 4444,
+  // path: "/wd/hub",
+  specs: ["./tests/*.ts"],
+  sync: true,
+  services: ["selenium-standalone"],
+  capabilities: [
+    {
+      browserName: "chrome",
+      "selenoid:options": {
+        enableVNC: true,
+        name: `Wdio ${require("./package.json").name}`
+      }
     }
-}
+  ],
+  baseUrl: "http://ip-5236.sunline.net.ua:38015",
+  framework: "mocha",
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 120000
+  },
+  before: function(capabilities, specs) {
+    process.env.TS_NODE_FILES = true;
+    require("ts-node").register();
+  }
+};
