603e1c29b707b842e7d4625ed530e108bfb49a97
Oleksandr Khotemskyi <xotabu4@gmail.com>
2019-03-05 17:40:01 +0200

    lesson 3 feedback and lesson 5 homework

---
+++ tests/lesson_3_homework/problems.js
@@ -0,0 +1,31 @@
+// ************************************************************************************
+// won't work in case empty array -
+
+let allDuckItems = [];
+let res = allDuckItems.every(duck => duck);
+console.log(res); // true
+
+it("should show results in case multiple items matches", function() {
+  searchWord("duck");
+  expect(browser.getUrl()).to.contain("query=duck");
+  expect($$(allDuckItems).every(duck => duck.isDisplayed())).to.equal(true);
+});
+// ************************************************************************************
+const duckPriceArr = $$(allDucks).map(duck =>
+  parseInt(duck.getAttribute("data-price"))
+);
+const duckPriceNewArr = duckPriceArr.slice(0); // copy array
+duckPriceNewArr.sort((a, b) => a - b); // price sorting
+
+// ************************************************************************************
+it("should show results in case multiple items matches", function() {
+  $('input[type="search"]').setValue("duck");
+  browser.pause(1000);
+  $('input[type="search"]').addValue("Enter");
+  browser.pause(1000);
+  // same locator on home and search results page. false positive test
+  expect($('main [class="col-xs-6 col-sm-4 col-md-3"]').isDisplayed()).to.equal(
+    true
+  );
+  // throw new Error("NOT IMPLEMENTED");
+});
---
+++ tests/lesson_5_homework/homework.ts
@@ -0,0 +1,38 @@
+/**
+ - Try to implement as much tests as you can
+ - Do not overload tests with logic, be simple
+ - browser.pause() NOT allowed, use browser.wait, browser.waitUntil
+ - You SHOULD use PageObjects for this tests
+ - prefer css selectors
+ - Use mocha before/after hooks to reuse pre/post conditions
+ - Use ChaiJS to make assertions
+ */
+
+// Each implemented test gives you 20 points
+describe("Cart", function() {
+  it("adding one item to cart should be successful", function() {
+    throw new Error("NOT IMPLEMENTED");
+  });
+
+  it("removing one item from cart should be successful", function() {
+    throw new Error("NOT IMPLEMENTED");
+  });
+
+  // from 1 to 2 for example
+  it("increasing item quantity in cart should be successful", function() {
+    throw new Error("NOT IMPLEMENTED");
+  });
+
+  // from 2 to 1 for example
+  it("decreasing item quantity in cart should be successful", function() {
+    throw new Error("NOT IMPLEMENTED");
+  });
+});
+
+// This test gives you 20 points
+describe("Prices", function() {
+  it("can be switched to EUR", function() {
+    throw new Error("NOT IMPLEMENTED");
+  });
+});
+
